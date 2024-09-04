9cb9e91f480a9823631faa248f4a78bab8a4cb63
adasilva <asilva5@xtec.cat>
2024-09-04 19:51:50 +0200

    Exemple 01

---
+++ exemples/01/exemple.py
@@ -0,0 +1,20 @@
+class Cotxe:
+    def __init__(self, model, any, color, en_venda):
+        self.model = model
+        self.any = any
+        self.color = color
+        self.en_venda = en_venda
+
+cotxe1 = Cotxe("Renault Megane", 2012, "verd", True)
+
+print(cotxe1)
+
+# print(cotxe1.model)
+# crea un segon cotxe i print els atributs
+# crea un tercer cotxe i print els atributs
+# mou la classe Cotxe a un fitxer cotxe.py
+# mètodes start i stop generics i personalitzats
+# mètode descriu
+# mètode per posar-lo o no en venda
+
+
