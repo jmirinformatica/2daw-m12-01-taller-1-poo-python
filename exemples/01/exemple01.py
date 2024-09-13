e5dbc137b76197b791f2971b89c218fd2ec48ad8
Alfonso da Silva <adasilva@tetrao.eu>
2024-09-13 20:05:37 +0200

    Exemple 1 finalitzat

--- exemples/01/exemple01.py
+++ exemples/01/exemple01.py
@@ -4,6 +4,7 @@ class Cotxe:
         self.any = any
         self.color = color
         self.en_venda = en_venda
+        self.quilometratge = 0
 
     def start(self):
         print(f"Cotxe {self.model} en marxa ")
@@ -11,14 +12,51 @@ class Cotxe:
     def stop(self):
         print(f"Cotxe {self.model} aturat")
 
+    def descripcio(self):
+        print("----------------------")
+        print(f"Model:{self.model}")
+        print(f"Any:{self.any}")
+        print(f"Color:{self.color}")
+        print(f"Quilometratge:{self.quilometratge}")
+        # if (self.en_venda):
+        #     print("En venda:SI")
+        # else:
+        #     print("En venda:NO")
+        print(f"En venda:{'SI' if self.en_venda else 'NO'}")
+        print("----------------------")
+
+    def posa_en_venda(self):
+        if (self.en_venda):
+            print("Ja hi soc en venda")
+        else:
+            self.en_venda = True
+            print(f"Cotxe {self.model} en venda!!!!")
+
+    def augmenta_quilometratge(self, q):
+        if (q < 0):
+            print("De que vas estafador!")
+        else:
+            self.quilometratge = self.quilometratge + q
+
 cotxe1 = Cotxe("Renault Megane", 2012, "verd", True)
 cotxe2 = Cotxe("Seat Toledo", 1987, "vermell", False)
+cotxe3 = Cotxe("Ford Fiesta", 1965, "groc", False)
 
-cotxe1.start()
-cotxe2.start()
+cotxe1.descripcio()
+cotxe1.augmenta_quilometratge(1000)
+cotxe1.descripcio()
+cotxe1.augmenta_quilometratge(-500)
+cotxe1.descripcio()
 
-cotxe2.stop()
-cotxe1.stop()
+
+
+# cotxe1.posa_en_venda()
+# cotxe1.descripcio()
+
+# cotxe1.descripcio()
+# cotxe2.descripcio()
+# cotxe3.descripcio()
+
 
 # print(cotxe1.model)
 # crea un segon cotxe i print els atributs
