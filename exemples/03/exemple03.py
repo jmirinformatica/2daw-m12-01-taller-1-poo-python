6f2c454a57ff89fe6ec94ea84e1739e9735f9b07
Alfonso da Silva <adasilva@tetrao.eu>
2024-09-20 17:39:23 +0200

    Exemple de super()

--- exemples/03/exemple03.py
+++ exemples/03/exemple03.py
@@ -14,6 +14,9 @@ class Animal(ABC):
     def dorm(self):
         print(f"{self.nom} està dormint")
 
+    def corre(self):
+        return f"{self.nom} està corrent"
+
 class Gos(Animal):
     def parla(self):
         print("Guau guau")
@@ -22,27 +25,47 @@ class Gat(Animal):
     def parla(self):
         print("Miau miau")
 
+class Cangur(Animal):
+    def parla(self):
+        print("Cangu cangu")
+
+    def corre(self):
+        resposta = super().corre()
+        nova_resposta = resposta + " i fa boing boing"
+        return nova_resposta
+
 class Porc(Animal):
+    def __init__(self, nom, pes):
+        super().__init__(nom) # soc un animal
+        self.pes = pes        # amb atributs propis
+
     def parla(self):
         print("Oink oink")
 
 # això dona error
 # un_animal = Animal("Epi") 
 
-un_gos = Gos("Rex")
+# un_gos = Gos("Rex")
 # un_gos.menja()
 # un_gos.parla()
 
-un_gat = Gat("Fifi")
+# un_gat = Gat("Fifi")
 # un_gat.dorm()
 # un_gat.parla()
 
-un_porc = Porc("Piggy")
+# un_porc = Porc("Piggy", 40) # el proc pesa 40 quilograms
 
-llista_animals = [un_gos, un_gat, un_porc]
+# un_gat = Gat("Fifi")
+# print(un_gat.corre())
 
-for a in llista_animals:
-    a.parla()
+un_cangur = Cangur("Popito")
+print(un_cangur.corre())
+
+
+# llista_animals = [un_gos, un_gat, un_porc]
+
+# for a in llista_animals:
+#     a.parla()
 
 # herència multi nivell: afegir classe EsserViu amb mètode Respira
 # herència múltiple: afegir classe Mascota amb mètode propietari
